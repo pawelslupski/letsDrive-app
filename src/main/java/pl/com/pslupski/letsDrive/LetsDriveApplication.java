9eb39bf59478c50e20aebb016018b62c60c1a4f6
pawelslupski <pawelslupski.go@gmail.com>
2021-02-21 13:21:24 +0100

    initial commit

---
+++ src/main/java/pl/com/pslupski/letsDrive/LetsDriveApplication.java
@@ -0,0 +1,13 @@
+package pl.com.pslupski.letsDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/pslupski/letsDrive/LetsDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.pslupski.letsDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
