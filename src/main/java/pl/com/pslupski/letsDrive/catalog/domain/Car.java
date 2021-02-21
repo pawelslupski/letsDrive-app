445c6ea93496aa9d7f9e69f01574f208f1f96f28
pawelslupski <pawelslupski.go@gmail.com>
2021-02-21 13:46:31 +0100

    car domain model&repo definitions

---
+++ src/main/java/pl/com/pslupski/letsDrive/catalog/domain/Car.java
@@ -0,0 +1,11 @@
+package pl.com.pslupski.letsDrive.catalog.domain;
+
+import java.math.BigDecimal;
+
+public class Car {
+    private Long id;
+    private String model;
+    private String year;
+    private BigDecimal price;
+    private int mileage;
+}
