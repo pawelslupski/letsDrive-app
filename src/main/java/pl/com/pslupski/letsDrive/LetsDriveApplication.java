92825c1c65ede9fc953e07471e75bb49c0f05591
pawelslupski <pawelslupski.go@gmail.com>
2021-05-02 14:06:44 +0200

    LD021/feature: cron job related configuration props delegated to the separate class

--- src/main/java/pl/com/pslupski/letsDrive/LetsDriveApplication.java
+++ src/main/java/pl/com/pslupski/letsDrive/LetsDriveApplication.java
@@ -2,11 +2,14 @@ package pl.com.pslupski.letsDrive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import pl.com.pslupski.letsDrive.order.application.OrdersProperties;
 
 @EnableScheduling
 @EnableJpaAuditing
+@EnableConfigurationProperties(OrdersProperties.class)
 @SpringBootApplication
 public class LetsDriveApplication {
 
--- src/main/java/pl/com/pslupski/letsDrive/order/application/AbandonedOrdersJob.java
+++ src/main/java/pl/com/pslupski/letsDrive/order/application/AbandonedOrdersJob.java
@@ -1,13 +1,15 @@
-package pl.com.pslupski.letsDrive.order.application.port;
+package pl.com.pslupski.letsDrive.order.application;
 
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import pl.com.pslupski.letsDrive.order.application.port.ModifyOrderUseCase;
 import pl.com.pslupski.letsDrive.order.db.OrderJpaRepository;
 import pl.com.pslupski.letsDrive.order.domain.Order;
 import pl.com.pslupski.letsDrive.order.domain.OrderStatus;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,12 +18,14 @@ import java.util.List;
 class AbandonedOrdersJob {
     private final OrderJpaRepository repository;
     private final ModifyOrderUseCase orderUseCase;
+    private final OrdersProperties properties;
 
     @Transactional
-    @Scheduled(fixedRate = 60_000)
+    @Scheduled(cron = "${app.orders.abandon-cron}")
     public void run() {
-        LocalDateTime timestamp = LocalDateTime.now().minusDays(2);
-        List<Order> orders = repository.findByStatusAndCreatedAtIsLessThanEqual(OrderStatus.NEW, timestamp);
+        Duration paymentPeriod = properties.getPaymentPeriod();
+        LocalDateTime olderThan = LocalDateTime.now().minus(paymentPeriod);
+        List<Order> orders = repository.findByStatusAndCreatedAtIsLessThanEqual(OrderStatus.NEW, olderThan);
         System.out.println("Found orders to be abandoned: " + orders);
         orders.forEach(order -> orderUseCase.updateOrderStatus(order.getId(), OrderStatus.ABANDONED));
     }
