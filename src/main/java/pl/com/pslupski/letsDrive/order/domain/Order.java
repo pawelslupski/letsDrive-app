88b5a0306acd099b337cf223d2550bbdd485db29
pawelslupski <pawelslupski.go@gmail.com>
2021-05-02 13:04:20 +0200

    LD019/feature: car parts quantity refresh on confirmed/revoked order

--- src/main/java/pl/com/pslupski/letsDrive/order/domain/Order.java
+++ src/main/java/pl/com/pslupski/letsDrive/order/domain/Order.java
@@ -5,11 +5,14 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import pl.com.pslupski.letsDrive.jpa.BaseEntity;
+import pl.com.pslupski.letsDrive.order.application.port.ModifyOrderUseCase;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+
+import static pl.com.pslupski.letsDrive.order.application.port.ModifyOrderUseCase.*;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -33,7 +36,9 @@ public class Order extends BaseEntity {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    public void updateStatus(OrderStatus newStatus) {
-        this.status = status.updateStatus(newStatus);
+    public UpdateStatusResult updateStatus(OrderStatus newStatus) {
+        UpdateStatusResult result = status.updateStatus(newStatus);
+        this.status = result.getNewStatus();
+        return result;
     }
 }
