80158d4d6b8d37669ccf23d516da8a69351df41e
pawelslupski <pawelslupski.go@gmail.com>
2021-08-12 20:54:20 +0200

    LD033/security: basic in memory users used for authentication

--- src/main/java/pl/com/pslupski/letsDrive/LetsDriveAppSecurityConfiguration.java
+++ src/main/java/pl/com/pslupski/letsDrive/LetsDriveAppSecurityConfiguration.java
@@ -2,6 +2,7 @@ package pl.com.pslupski.letsDrive;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,5 +17,17 @@ public class LetsDriveAppSecurityConfiguration extends WebSecurityConfigurerAdap
                 .and().httpBasic()
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("marek@example.org")
+                    .password("{noop}xxx")
+                    .roles("USER")
+                .and()
+                .withUser("admin")
+                    .password("{noop}xxx")
+                    .roles("ADMIN");
     }
 }
